0fb45aa01bf6c67a2b2ee1c079bb3f49ad46bd63
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-05-12 16:36:46 -0600

    adds readme.md

--- src/client/screens/Auth.jsx
+++ src/client/screens/Auth.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Auth() {
+    const [register, setRigster] = useState(false)
+
+    const toggle = () => setRigster(!register)
+
     return (
         <section>
             <h1>Auth</h1>
+            <form>
+                <input type="text" placeholder="Username"></input>
+                <input type="password" placeholder="Password"></input>
+                <button>{register ? "Register" : "Login"}</button>
+            </form>
+            <button>Click here to {!register ? "Register" : "Login"}</button>
         </section>
     )
 }
