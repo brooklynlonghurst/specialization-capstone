b823deef8b2c0dfc2bf011f066dbdb4f88ccb706
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-05-17 11:59:18 -0600

    added font

--- src/client/screens/Home.jsx
+++ src/client/screens/Home.jsx
@@ -7,6 +7,8 @@ function Home() {
         <section>
             <h1>HOME OF CONSTELLION KIDS</h1>
             <h3>Welcome, {state.username}</h3>
+            <img src='https://s3.us-east-1.amazonaws.com/s3.discoveryplace.org/craft3/_mediumWidth/Constellations-shutterstock_267383846.jpg' alt='constellation img'/>
+            
         </section>
     )
 }
