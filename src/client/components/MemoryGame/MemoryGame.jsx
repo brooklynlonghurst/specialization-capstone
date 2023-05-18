a45762126afdb8fb49d375fd2d6a599ee63b12fc
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-05-18 17:27:48 -0600

    memory game

--- src/client/components/MemoryGame/MemoryGame.jsx
+++ src/client/components/MemoryGame/MemoryGame.jsx
@@ -16,9 +16,11 @@ function MemoryGame() {
     const [choiceOne, setChoiceOne] = useState(null)
     const [choiceTwo, setChoiceTwo] = useState(null)
     const [disabled, setDisabled] = useState(false)
+    const [showFinalResults, setFinalResults] = useState(false)
 
-    const shuffleCards = () => {
-        const shuffledCards = [...cardImages, ...cardImages]
+
+    const shuffleCards = async () => {
+        const shuffledCards = await [...cardImages, ...cardImages]
             .sort(() => Math.random() - 0.5)
             .map((card) => ({ ...card, id: Math.random()}))
 
@@ -27,25 +29,37 @@ function MemoryGame() {
         setCards(shuffledCards)
         setTurns(0)
     }
-
-    const handleChoice = (card) => {
-        choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
-    }
+        console.log(showFinalResults)
+        const handleChoice = (card) => {
+            choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+        }
 
     useEffect(() => {
         if(choiceOne && choiceTwo) {
             setDisabled(true)
             if(choiceOne.src === choiceTwo.src) {
-                setCards(prevCards => {
-                    return prevCards.map(card => {
+                let newCards = cards.map(card => {
                         if(card.src === choiceOne.src) {
                             return {...card, matched: true}
                         } else {
                             return card
                         }
                     })
-                })
-                resetTurn()
+                setCards(newCards)
+                    let matches = newCards.reduce((acc, cur) => {
+                        if(cur.matched){
+                            return acc + 1
+                        } else {
+                            return acc 
+                        }
+                    }, 0)
+                    console.log(matches, newCards.length)
+                if(matches === newCards.length) {
+                    console.log("finished")
+                    return setTimeout(() => setFinalResults(true), 1000)
+                } else {
+                    resetTurn()
+                }
             } else {
                 setTimeout(() => resetTurn(), 1000)
                 
@@ -64,22 +78,34 @@ function MemoryGame() {
         shuffleCards()
     }, [])
 
+    const newGame = async () => {
+        setFinalResults(false)
+        // await shuffleCards()
+        // resetTurn()
+    }
     return (
         <section>
             <h2>Memory Game</h2>
             <h3>Turns: {turns}</h3>
-            <button onClick={shuffleCards}>New Game</button>
-            <div className="card-grid">
-                {cards.map(card => (
-                    <SingleCard 
-                        key={card.id} 
-                        card={card}
-                        handleChoice={handleChoice}
-                        flipped={card === choiceOne || card === choiceTwo || card.matched}
-                        disabled={disabled}
-                    />
-                ))}
-            </div>
+            {showFinalResults ? (
+                <div className='final-results'>
+                <h1>Final Results</h1>
+                <h2>{turns} Turns to find all the matches</h2>
+                    <button onClick={newGame}>New Game</button>
+                </div>
+            ) : (
+                <div className="card-grid">
+                    {cards.map(card => (
+                        <SingleCard 
+                            key={card.id} 
+                            card={card}
+                            handleChoice={handleChoice}
+                            flipped={card === choiceOne || card === choiceTwo || card.matched}
+                            disabled={disabled}
+                        />
+                    ))}
+                </div>
+            )}
         </section>
     )
 }
