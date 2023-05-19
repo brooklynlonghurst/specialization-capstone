4179d07a4e3424f9b659e1fd9db74a35f3420d9c
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-05-19 13:02:42 -0600

    home page button functional

--- src/client/components/GameCard.jsx
+++ src/client/components/GameCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function GameCard(props) {
+    const navigate = useNavigate()
     return (
         <div className='game-card-container'>
         <div className='game-card'>
             <h2>{props.title}</h2>
             {props.img}
-            <button>Play Game</button>
+            <button onClick={() => navigate('/games')}>Play Game</button>
         </div>
         </div>
     )
