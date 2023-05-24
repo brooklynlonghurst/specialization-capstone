cf7f1feb74288b5ec973c8b80823e38f80e6f0b7
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-05-24 11:42:06 -0600

    finished card for drawing

--- src/client/screens/Home.jsx
+++ src/client/screens/Home.jsx
@@ -13,10 +13,10 @@ function Home() {
                 <p className='intro-p'><IoPlanet size='15px'/>Constellation Kids is a place for you to learn all about the stars! Play games and write posts of what you've learned about constellations. Check your progress on your scoreboard. Go explore the world that is right above you!</p>
             </div>
             <div className='home-page-card'>
-                {/* <img    */}
+                {/* <img src='https://s3.us-east-1.amazonaws.com/s3.discoveryplace.org/craft3/_mediumWidth/Constellations-shutterstock_267383846.jpg' alt='constellation img'/>   */}
                 <GameCard title={'Memory Game'} img={<img className='game-card-img' src='./img/memory.png'/>}/>
                 <GameCard title={'Quiz'} img={<img className='game-card-img' src='./img/quiz.png' />} />  
-                <GameCard title={'Create Your Constellation'} img={<img className='game-card-img' src='https://s3.us-east-1.amazonaws.com/s3.discoveryplace.org/craft3/_mediumWidth/Constellations-shutterstock_267383846.jpg' alt='constellation img'/> } />        
+                <GameCard title={'Create Your Constellation'} img={<img className='game-card-img' src='./img/drawing.png' />} />        
             </div>
         </div>
     )
