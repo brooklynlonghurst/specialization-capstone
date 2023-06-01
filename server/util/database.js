e4466e40c780d573e45ca44692d6bf84237d9248
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-06-01 15:09:13 -0600

    database url

--- server/util/database.js
+++ server/util/database.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require('sequelize')
 require('dotenv').config()
-const DATABASE_URL = process.env.DATABASE_URL
+const DATABASE_URL = process.env.DATABASE_URL + "?sslmode=require"
 
 const db = new Sequelize(
     DATABASE_URL, {
